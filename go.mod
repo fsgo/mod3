adbc1e31a39b7defd28177f965cfa4b53e9de3b5
hidu <duv123+github@gmail.com>
2020-07-03 23:28:32 +0800

    1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fsgo/mod3
+
+go 1.14
+
+require github.com/fsgo/mod1 v1.0.1
---
+++ version.go
@@ -0,0 +1,17 @@
+/*
+ * Copyright(C) 2020 github.com/hidu  All Rights Reserved.
+ * Author: hidu (duv123+git@baidu.com)
+ * Date: 2020/7/3
+ */
+
+package mod2
+
+import (
+	"runtime"
+
+	"github.com/fsgo/mod1"
+)
+
+func Version() string {
+	return mod1.Version() + "\n" + "mod3{ mod_version:1.0.0 go:" + runtime.Version() + " }"
+}
